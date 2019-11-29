507a1ce978ebdb555a88debc5e8c0183578e644d
Dazpel <alexxdav97@gmail.com>
2019-11-29 01:12:59 -0500

    lala

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import React from "react";
-import "./App.css";
-import { CardList } from "./components/cardlist/cardlist.component";
-import { Card } from "./components/card/card.component";
-import { SearchBox } from "./components/searchbox/searchbox.component";
-
+import React from 'react'
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [],
-      searchField: ""
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
-  }
-
-  handleChange = (event) => {
-    this.setState({searchField: event.target.value})
-  }
-
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-
     return (
-      <div className="App">
-        <h1>Roblox</h1>
-        <SearchBox 
-        placeholder='Search Monsters'
-        handleChange={this.handleChange} />
-        <CardList 
-        monsters={filteredMonsters} />
+      <div>
+        <h1>Home</h1>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import Personas from "./components/pages/Personas";
 import Planets from "./components/pages/Planets";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
@@ -8,21 +9,23 @@ import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 const routing = (
   <Router>
     <div className="base">
-    <h1>Select Which Data to Fetch:</h1>
-      
-          <Link to="/planets">
-              <button>Planets</button>
-          </Link>
-      
-          <Link to="/personas">
-          <button>Personas</button>
-          </Link>
-      
-      <Route exact path="/planets" component={Planets} />
-      <Route exact path="/personas" component={Personas} />
+      <h1>Select Which Data to Fetch:</h1>
+
+      <Link to="/">Home</Link>
+
+      <Link to="/planets">
+        <button>Planets</button>
+      </Link>
+
+      <Link to="/personas">
+        <button>Personas</button>
+      </Link>
+
+      <Route exact path="/" component={App} />
+      <Route path="/planets" component={Planets} />
+      <Route path="/personas" component={Personas} />
     </div>
   </Router>
 );
 
-
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
