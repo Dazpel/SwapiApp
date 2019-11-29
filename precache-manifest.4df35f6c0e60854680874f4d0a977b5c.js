216451271b3aa1a6f014a96663b9a003f882467e
Dazpel <alexxdav97@gmail.com>
2019-11-29 00:15:27 -0500

    Updates

---
+++ precache-manifest.4df35f6c0e60854680874f4d0a977b5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437c789e3e1a4955811e1721b9eaadc4",
+    "url": "/Dazpel/SwapiApp/index.html"
+  },
+  {
+    "revision": "717a019d2c78d2d97b54",
+    "url": "/Dazpel/SwapiApp/static/css/main.2d45bef2.chunk.css"
+  },
+  {
+    "revision": "0306df38bfc62c983d93",
+    "url": "/Dazpel/SwapiApp/static/js/2.0f32d3fa.chunk.js"
+  },
+  {
+    "revision": "717a019d2c78d2d97b54",
+    "url": "/Dazpel/SwapiApp/static/js/main.fa3d3dd0.chunk.js"
+  },
+  {
+    "revision": "9d9e2e9d332f4439fe45",
+    "url": "/Dazpel/SwapiApp/static/js/runtime-main.1d67b4ba.js"
+  }
+]);
